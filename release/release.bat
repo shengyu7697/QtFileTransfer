d36bb52720f7d70ca68de287d4276f326884d38b
shengyu <shengyu7697@gmail.com>
2018-06-12 17:59:27 +0800

    add release script

---
+++ release/release.bat
@@ -0,0 +1,15 @@
+windeployqt.exe FileTransferServer.exe ^
+--no-quick-import ^
+--no-translations ^
+--no-system-d3d-compiler ^
+--no-angle ^
+--no-opengl-sw
+
+windeployqt.exe FileTransferClient.exe ^
+--no-quick-import ^
+--no-translations ^
+--no-system-d3d-compiler ^
+--no-angle ^
+--no-opengl-sw
+
+pause
