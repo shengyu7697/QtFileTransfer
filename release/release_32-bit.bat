6cc79270043fd1c41404f622c24701bd0962ae12
shengyu <shengyu7697@gmail.com>
2018-06-13 10:44:02 +0800

    update release script

--- release/release_32-bit.bat
+++ release/release_32-bit.bat
@@ -1,11 +1,11 @@
-windeployqt.exe FileTransferServer.exe ^
+C:\Qt\Qt5.10.0\5.10.0\msvc2015\bin\windeployqt.exe FileTransferServer.exe ^
 --no-quick-import ^
 --no-translations ^
 --no-system-d3d-compiler ^
 --no-angle ^
 --no-opengl-sw
 
-windeployqt.exe FileTransferClient.exe ^
+C:\Qt\Qt5.10.0\5.10.0\msvc2015\bin\windeployqt.exe FileTransferClient.exe ^
 --no-quick-import ^
 --no-translations ^
 --no-system-d3d-compiler ^
---
+++ release/release_64-bit.bat
@@ -0,0 +1,15 @@
+C:\Qt\Qt5.10.0\5.10.0\msvc2015_64\bin\windeployqt.exe FileTransferServer.exe ^
+--no-quick-import ^
+--no-translations ^
+--no-system-d3d-compiler ^
+--no-angle ^
+--no-opengl-sw
+
+C:\Qt\Qt5.10.0\5.10.0\msvc2015_64\bin\windeployqt.exe FileTransferClient.exe ^
+--no-quick-import ^
+--no-translations ^
+--no-system-d3d-compiler ^
+--no-angle ^
+--no-opengl-sw
+
+pause
